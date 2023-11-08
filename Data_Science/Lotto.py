548d630dd8640dbd87db9aa2ab91c4c5d57a1986
Jihoon Choi <aa09302003@gmail.com>
2023-11-08 15:36:22 +0900

    update lotto

--- Data_Science/Lotto.py
+++ Data_Science/Lotto.py
@@ -10,6 +10,7 @@ np.random.seed(int(time.time()))
 # np.random.seed(3)
 Lotto_table = Table.read_table('Data_Science/Data/Lotto.CSV')
 Sample = Lotto_table.take(make_array(np.random.randint(0, 1091)))
+Lotto_table = ""
 Lotto_1st_list = []
 for i in range(1, 7):
   Lotto_data = Sample.column(i)
@@ -21,31 +22,42 @@ count_1st = Sample.column(8)[0]
 print(buyCount)
 print(count_1st)
 win_cnt = 0
+cnt = 0
 
-def win_lotto():
+def win_lotto(win_cnt):
   if win_cnt > 0:
     return 1
   return -1
 
-def win_lotto_count(lotto, num_sel):
-  if lotto == num_sel:
+def win_lotto_count(num_sel, win_cnt, cnt):
+  if Lotto_1st_list == num_sel:
     win_cnt = win_cnt +1
+  cnt = cnt + 1
+  if (cnt == (int)(buyCount/4)):
+    print(cnt, time.time())
+  elif (cnt == (int)(buyCount/4*2)):
+    print(cnt, time.time())
+  elif (cnt == (int)(buyCount/4*3)):
+    print(cnt, time.time())
 
 def sampling():
   for i in np.arange(buyCount):
     num_sel_temp = np.random.choice(a=45, size=6, replace=False)
     num_sel = [(b+1) for b in num_sel_temp]
     num_sel = sorted(num_sel)
-    win_lotto_count(Lotto_1st_list, num_sel)
+    win_lotto_count(num_sel, win_cnt, cnt)
     num_sel = []
 
 thread_list = []
-for i in range(2000):
-  thread = threading.Thread(target = sampling())
+print(cnt, time.time())
+for i in range(10):
+  thread = threading.Thread(target = sampling)
   thread_list.append(thread)
+  thread.start()
 
-for i in range(2000):
-  thread_list[i].start()
+for thread in thread_list:
+  thread.join()
 
-print(win_lotto())
+print(cnt)
+print(win_lotto(win_cnt))
 print(win_cnt)
