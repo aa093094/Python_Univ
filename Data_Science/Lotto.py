d356990b78fadb3d5a5226d4cd109611db06fce4
Jihoon Choi <aa09302003@gmail.com>
2023-11-10 14:18:50 +0900

    update lotto

--- Data_Science/Lotto.py
+++ Data_Science/Lotto.py
@@ -10,67 +10,86 @@ plots.style.use('fivethirtyeight')
 np.random.seed(int(time.time()))
 # np.random.seed(3)
 Lotto_table = Table.read_table('Data_Science/Data/Lotto.CSV')
-Sample = Lotto_table.take(make_array(np.random.randint(0, 1091)))
+number = np.random.randint(0, 100)
+num_list = []
+if (number > 90):
+  num_list.append(0)
+  for i in range(0, 10):
+    temp = number + 100 * i
+    num_list.append(temp)
+else:
+  for i in range(0, 11):
+    temp = number + 100 * i
+    num_list.append(temp)
+Sample_list = []
+for nums in num_list:
+  Sample = Lotto_table.take(make_array(nums))
+  Sample_list.append(Sample)
 Lotto_table = ""
-Lotto_1st_list = []
-for i in range(1, 7):
-  Lotto_data = Sample.column(i)
-  Lotto_1st_list.append(Lotto_data[0])
-index = Sample.column(0)[0]
-print(index)
-buyCount = Sample.column(10)[0].astype(int)
-count_1st = Sample.column(8)[0]
-print(buyCount)
-print(count_1st)
-win_cnt = 0
-cnt = 0
-current_time = datetime.datetime.now()
-formatted_time = current_time.strftime("%H:%M")
+index_list = []
+real_win_cnt_list = []
+sample_win_cnt_list = []
+buyCount_list = []
+sampleCount_list = []
 
-def win_lotto(win_cnt):
-  if win_cnt > 0:
-    return 1
-  return -1
+for j in range(0, 11):
+  Lotto_1st_list = []
+  for i in range(1, 7):
+    Lotto_data = Sample_list[j].column(i)
+    Lotto_1st_list.append(Lotto_data[0])
+  index = Sample_list[j].column(0)[0]
+  print(index)
+  index_list.append(index)
+  buyCount = Sample_list[j].column(10)[0].astype(int)
+  count_1st = Sample_list[j].column(8)[0]
+  print(buyCount)
+  print(count_1st)
+  buyCount_list.append(buyCount)
+  real_win_cnt_list.append(count_1st)
+  win_cnt = 0
+  cnt = 0
+  current_time = datetime.datetime.now()
+  formatted_time = current_time.strftime("%H:%M")
 
-def win_lotto_count(num_sel, win_cnt, cnt):
-  if Lotto_1st_list == num_sel:
-    win_cnt = win_cnt +1
-  cnt = cnt + 1
-  """
-  if (cnt == (int)(buyCount/4)):
-    print(cnt, formatted_time)
-  elif (cnt == (int)(buyCount/4*2)):
-    print(cnt, formatted_time)
-  elif (cnt == (int)(buyCount/4*3)):
-    print(cnt, formatted_time)
-  """
+  def sampling(Count):
+    global cnt
+    global win_cnt
+    for i in np.arange(Count):
+      num_sel_temp = np.random.choice(a=45, size=6, replace=False)
+      num_sel = [(b+1) for b in num_sel_temp]
+      num_sel = sorted(num_sel)
+      if (Lotto_1st_list == num_sel):
+        win_cnt = win_cnt + 1
+      num_sel = []
+      cnt = cnt + 1
+      if (cnt % 10000000 == 0):
+        current_time = datetime.datetime.now()
+        formatted_time = current_time.strftime("%H:%M")
+        print(win_cnt, cnt, formatted_time)
+      
+  thread_list = []
+  print(cnt, formatted_time)
+  np.random.seed(int(time.time()))
+  for i in range(2000):
+    thread = threading.Thread(target = sampling(buyCount/2000))
+    thread_list.append(thread)
+    thread.start()
 
-def sampling(Count):
-  global cnt
-  global win_cnt
-  for i in np.arange(Count):
-    num_sel_temp = np.random.choice(a=45, size=6, replace=False)
-    num_sel = [(b+1) for b in num_sel_temp]
-    num_sel = sorted(num_sel)
-    if (Lotto_1st_list == num_sel):
-      win_cnt = win_cnt + 1
-    num_sel = []
-    cnt = cnt + 1
-    if (cnt % 10000000 == 0):
-      current_time = datetime.datetime.now()
-      formatted_time = current_time.strftime("%H:%M")
-      print(win_cnt, cnt, formatted_time)
-    
-thread_list = []
-print(cnt, formatted_time)
-for i in range(2000):
-  thread = threading.Thread(target = sampling(buyCount/2000))
-  thread_list.append(thread)
-  thread.start()
+  for thread in thread_list:
+    thread.join()
 
-for thread in thread_list:
-  thread.join()
+  current_time = datetime.datetime.now()
+  formatted_time = current_time.strftime("%H:%M")
+  print(win_cnt, cnt, formatted_time)
+  sample_win_cnt_list.append(win_cnt)
+  sampleCount_list.append(cnt)
 
-current_time = datetime.datetime.now()
-formatted_time = current_time.strftime("%H:%M")
-print(win_cnt, cnt, formatted_time)
+All_table = Table().with_columns(
+  'index', index_list,
+  'Real_win', real_win_cnt_list,
+  'Real_cnt', buyCount_list,
+  'Sample_win', sample_win_cnt_list,
+  'Sample_cnt', sampleCount_list
+)
+
+All_table.to_csv('Sampling1.csv')
