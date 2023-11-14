e974114360fe5ff8b49fbdaa732fa6a972d19868
Jihoon Choi <aa09302003@gmail.com>
2023-11-14 17:07:37 +0900

    update lotto_dist

--- Data_Science/Lotto_Distribution.py
+++ Data_Science/Lotto_Distribution.py
@@ -2,7 +2,17 @@ from datascience import *
 import numpy as np
 
 import matplotlib.pyplot as plots
+import matplotlib
+matplotlib.use('TkAgg')
 plots.style.use('fivethirtyeight')
+
+Lotto_table = Table.read_table('Data_Science/Data/Lotto.CSV')
+reversed_table = Lotto_table.sort('회차')
+win_cnt_list = reversed_table.column(8)
+buyCount_list = reversed_table.column(10).astype(int)
+index_list = reversed_table.column(0)
+Lotto_table = ""
+reversed_table = ""
 
 Sample_list0 = []
 Sample_list1 = []
@@ -16,7 +26,7 @@ Sample_list8 = []
 Sample_list9 = []
 Sample_list10 = []
 
-for i in range(2):
+for i in range(3):
     Sample_Table = Table.read_table('Data_Science/Data/Lotto_Sampling' + str(i) + '.csv')
     Win_Data = Sample_Table.column(3)
     Sample_list0.append(Win_Data[0])
@@ -30,4 +40,39 @@ for i in range(2):
     Sample_list8.append(Win_Data[8])
     Sample_list9.append(Win_Data[9])
     Sample_list10.append(Win_Data[10])
+
+sum = 0
+for cnt in win_cnt_list:
+    sum = sum + cnt
+avg = sum / len(win_cnt_list)
+print(avg)
+
+win_cnt_table = Table().with_columns(
+    'index', index_list,
+    'win_cnt', win_cnt_list
+)
+
+buyCount_table = Table().with_columns(
+    'index', index_list,
+    'buyCount', buyCount_list
+)
+
+win_cnt_table.plot('index', 'win_cnt', linewidth = 0.5)
+plots.xlabel('Index')
+plots.ylabel('Win_Cnt')
+plots.title('Win_Cnt_Table')
+plots.show()
+
+buyCount_table.plot('index', 'buyCount', linewidth = 0.5)
+plots.xlabel('Index')
+plots.ylabel('BuyCount')
+plots.title('BuyCount_Table')
+plots.show()
+
+
+
+
+
+
+
 
